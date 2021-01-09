1ed76c81a7edcd03d845e68487f3ae64d86b2741
Artem Chirtsov <chirartem@gmail.com>
2021-01-09 16:24:16 +0300

    first commit

---
+++ barometer_bme280.ino
@@ -0,0 +1,37 @@
+#include "Seeed_BME280.h"
+#include <Wire.h>
+ 
+BME280 bme280;
+ 
+void setup()
+{
+  Serial.begin(115200);
+  if(!bme280.init()){
+    Serial.println("Device error!");
+  }
+}
+ 
+void loop()
+{
+ 
+  Serial.println("{");
+  //get and print temperatures
+  Serial.print("\"t\": \"");
+  Serial.print(bme280.getTemperature());
+  Serial.println("\",");
+ 
+  //get and print atmospheric pressure data
+  Serial.print("\"p\": \"");
+  Serial.print(bme280.getPressure());
+  Serial.println("\",");
+
+  //get and print humidity data
+  Serial.print("\"h\": \"");
+  Serial.print(bme280.getHumidity());
+  Serial.println("%\"");
+  
+  Serial.println("}");
+  Serial.println();
+ 
+  delay(1000);
+}
